5ae55241f731e06086f64f1c1badbe16fecc2fc6
Love-Kogasa <dachao_88@qq.com>
2026-06-19 18:43:42 +0800

    优化细节，暂未加入实际功能，优化手机端界面，增加消息提醒，增加一点点屎山代码

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,12 +4,22 @@ export default function Navbar(){
     return <div style='height: 30px;'>
       <nav class='demo'>
         <a class='brand' onClick={() => linkTo('/')}>
-          <span>Lapis Net Data Editor</span>
+          <span>
+            <span class='bi-code-slash kongge'></span>
+            <>Lapis Net Data Editor</>
+          </span>
         </a>
         <input id="bmenub" type="checkbox" class="show" />
         <label for="bmenub" class="burger pseudo button">&#8801;</label>
         <div class='menu'>
-            <a onClick={() => linkTo('/')}>Home</a>
+            <button class='pseudo' onClick={() => window.open('https://github.com/LapisNet/team-data-editor')}>
+              <span class='bi-github kongge'></span>
+              <>Github</>
+            </button>
+            <button class='pseudo' onClick={() => linkTo('/')}>
+              <span class='bi-house kongge'></span>
+              <>Home</>
+            </button>
         </div>
       </nav>
     </div>
